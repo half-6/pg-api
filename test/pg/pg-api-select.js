eca0ca1b02c5a6c227efb15edbec4c714d978139
Cyokin <Jun.Zhang@hibu.com>
2017-04-28 23:12:27 -0700

    add default support

--- test/pg/pg-api-select.js
+++ test/pg/pg-api-select.js
@@ -6,6 +6,18 @@
 const $select= require('./../resource/pg/select.json');
 describe('Unit Test -- api/pg-api.js(select)',function () {
     describe('select api', ()=> {
+        it('select default', (done)=> {
+            $chai.request(global.$app)
+                .get(`/api/db/user`)
+                .end(function (err,res) {
+                    (err == null).should.be.true;
+                    res.should.have.status(200);
+                    res.should.be.a.json;
+                    $logger.info(JSON.stringify(res.body) );
+                    res.body.should.have.property('response');
+                    done();
+                })
+        });
         it('select table', (done)=> {
             $chai.request(global.$app)
                 .get(`/api/db/user/1`)
@@ -33,7 +45,7 @@ describe('Unit Test -- api/pg-api.js(select)',function () {
         });
         it('error test', (done)=> {
             $chai.request(global.$app)
-                .get(`/api/db/user`)
+                .get(`/api/db/unknowtable`)
                 //.query({$q:JSON.stringify($select)})
                 .end(function (err,res) {
                     (err == null).should.be.false;
