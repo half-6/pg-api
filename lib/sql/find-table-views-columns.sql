bcb517aa8e1252555a8261afedd097194804559e
Cyokin <Jun.Zhang@hibu.com>
2017-07-13 07:54:17 -0700

    fix unique id issue

--- lib/sql/find-table-views-columns.sql
+++ lib/sql/find-table-views-columns.sql
@@ -1,10 +1,10 @@
 SELECT
   CASE WHEN table_type = 'BASE TABLE' THEN ccu.column_name = tc.column_name
-    WHEN table_type = 'VIEW' THEN tc.ordinal_position = 1
-    ELSE FALSE
+  WHEN table_type = 'VIEW' THEN tc.ordinal_position = 1
+  ELSE FALSE
   END AS is_primary_key,
   CASE WHEN data_type = 'ARRAY' THEN concat(substring(udt_name,2) , '[]')
-    ELSE udt_name
+  ELSE udt_name
   END AS type,
   tc.*,
   tb.table_type
@@ -17,4 +17,5 @@ FROM information_schema.columns tc
               ) tb
     on tc.table_schema = tb.table_schema AND tb.table_name = tc.table_name
   LEFT JOIN information_schema.constraint_column_usage ccu
-    ON tc.table_schema =ccu.table_schema AND tc.table_name = ccu.table_name;
+    ON tc.table_schema =ccu.table_schema AND tc.table_name = ccu.table_name
+ORDER BY tc.table_schema, tc.table_name, tc.ordinal_position;
